810288f9dafd39cc87ec0ddd2761060d154b7916
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-09-09 15:50:49 +0200

    Change Namespace

---
+++ src/Providers/EscoolServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Scool\Settings\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class EscoolServiceProvider.
+ */
+class EscoolSettingsServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->app->register('Acacha\Profile\Providers\ProfileServiceProvider');
+    }
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+}
